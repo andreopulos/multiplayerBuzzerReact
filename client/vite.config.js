ea8fed5b238ce5f044268f54d9d4eb3bb983c7bd
Andrea.Roccaro <andrea.roccaro@mavriq.com>
2026-02-19 16:36:58 +0100

    deploy 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
